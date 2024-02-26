a63d780da694e0534df426f7dd917140239eb5fe
ovi1173 <ohiduzzaman6422@gmail.com>
2024-02-26 11:29:02 +0600

    finishing keyboard and its func section

--- alpha-clash-pro/scripts/alpha-clash.js
+++ alpha-clash-pro/scripts/alpha-clash.js
@@ -1,13 +1,24 @@
 
-    //step-1: hide the home screen to hide the screen ad
-    // step-2: show the playground
-    // const homeSection = document.getElementById('home-screen');
-    // homeSection.classList.add('hidden');
+//step-1: hide the home screen to hide the screen ad
+// step-2: show the playground
+// const homeSection = document.getElementById('home-screen');
+// homeSection.classList.add('hidden');
 
-    // const playgroundSection = document.getElementById('play-ground');
-    // playgroundSection.classList.remove('hidden');
+// const playgroundSection = document.getElementById('play-ground');
+// playgroundSection.classList.remove('hidden');
 
-function play(){
+function continueGame() {
+  // step-1: generate a random alphabet
+  const alphabet = getARandomAlphabet();
+  //show it
+  console.log('Your random alphabet',alphabet);
+  const currentAlphabetElement = document.getElementById('current-alphabet') 
+  currentAlphabetElement.innerText = alphabet;
+  setBackgroundColorById(alphabet);
+
+}
+function play() {
     hideElementById('home-screen');
-    showElementById('play-ground')
+    showElementById('play-ground');
+    continueGame();
 }
